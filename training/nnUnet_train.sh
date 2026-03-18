7b209dc63650dcc344edde3f6bd6dc3263621119
Walid Bouhazza <walid.bouhazza@yahoo.com>
2026-03-18 00:52:47 -0400

    "fix: update SLURM script with scratch paths and all 5 folds

--- training/nnUnet_train.sh
+++ training/nnUnet_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=nnUnet_train
-#SBATCH --time=04:00:00
+#SBATCH --time=48:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=8G
@@ -18,19 +18,30 @@ source $SLURM_TMPDIR/venv/bin/activate
 
 pip install -r requirements.txt 
 
-echo -e "\nSetting up the nnUnet variables..."
-export nnUNet_raw="$HOME/projects/ift3710/nnUNet_raw"
-export nnUNet_preprocessed="$HOME/projects/ift3710/nnUNet_preprocessed"
-export nnUNet_results="$HOME/projects/ift3710/nnUNet_results"
+echo -e "\nSetting up nnUNet variables..."
+export nnUNet_raw="/scratch/$USER/nnUNet_raw"
+export nnUNet_preprocessed="/scratch/$USER/nnUNet_preprocessed"
+export nnUNet_results="/scratch/$USER/nnUNet_results"
  
 mkdir -p /scratch/$USER/logs
  
 echo "BEGIN : $(date)"
 
-python $HOME/projects/ift3710/preprocessing/convert_to_nnunet.py
+python preprocessing/convert_to_nnunet.py \
+    --task-dir /scratch/$USER/data/Task02_Heart \
+    --images-tr-out /scratch/$USER/nnUNet_raw/Dataset002_Heart/imagesTr \
+    --labels-tr-out /scratch/$USER/nnUNet_raw/Dataset002_Heart/labelsTr \
+    --images-ts-out /scratch/$USER/nnUNet_raw/Dataset002_Heart/imagesTs \
+    --output-json /scratch/$USER/nnUNet_raw/Dataset002_Heart/dataset.json
+    
 nnUNetv2_plan_and_preprocess -d 2 --verify_dataset_integrity
  
-nnUNetv2_train 2 3d_fullres 0 
+nnUNetv2_train 2 3d_fullres 0 --npz
+nnUNetv2_train 2 3d_fullres 1 --npz
+nnUNetv2_train 2 3d_fullres 2 --npz
+nnUNetv2_train 2 3d_fullres 3 --npz
+nnUNetv2_train 2 3d_fullres 4 --npz
+ 
 
 echo "END : $(date)"
  
